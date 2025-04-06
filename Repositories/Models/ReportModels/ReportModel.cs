3ddb42d009144ab3861aecb91e1c10e45ab81315
Thanh Dung <nguyenthanhdung2308@gmail.com>
2025-04-06 22:31:30 +0700

    Clean project, remove .vs, ignore build files

---
+++ Repositories/Models/ReportModels/ReportModel.cs
@@ -0,0 +1,20 @@
+﻿using Repositories.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Models.ReportModels
+{
+    public class ReportModel
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string? FileUrl { get; set; }
+        public Guid ReporterId { get; set; }
+        public Guid ArtworkId { get; set; }
+        public ReportStatus Status { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
